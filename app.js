556d05f601021b22eee44e7825d85f90ce360e6e
manbropig <jamie@thestorefront.com>
2016-01-24 10:28:49 -0800

    Emit Signal When a Dataset Is Downloaded

--- app.js
+++ app.js
@@ -3,17 +3,55 @@
 'use strict';
 var Resizer = require('./app/components/resizer');
 var Downloader = require('./app/components/downloader');
+var EventEmitter = require('events');
+var async = require('async');
 var inputCSVFile = 'data/image_urls.csv';
 // var Buffer = require('bugger');
 
 (function() {
+  var emitter = new EventEmitter();
   //init tools
   var resizer = new Resizer();
-  var downloader = new Downloader();
+  var downloader = new Downloader(emitter, 5);
 
   //collect urls from a csv
   //keep track of listing_id association
-  downloader.parse(inputCSVFile);
+  // downloader.parse(inputCSVFile);
+  downloader.readCSV(inputCSVFile);
+  emitter.on('csv read complete', () => {
+    async.waterfall([
+      (callback) => {
+        //NOTE: here instead call downloader.download or something which calls
+        //getNextData, downloads all the images in the set,
+        //then emits a 'send next data set' event
+        //or 'done' event if done
+        downloader.download();
+
+      }
+    ])
+  });
+
+
+  emitter.on('request set complete', () => {
+    console.log('first complete');
+  });
+
+  // async.waterfall([
+  //   function(callback) {
+  //     //first read all lines of the file
+  //     downloader.readCSV(inputCSVFile, function() {
+  //       callback(null, downloader.csvData);
+  //     });
+  //
+  //   },
+  //   function(csvData, callback) {
+  //
+  //     console.log(csvData.length);
+  //
+  //   },
+  //
+  // ]);
+
   //download images based on urls
   //once a file is downloaded, emit event with file location
 
--- app/components/downloader.js
+++ app/components/downloader.js
@@ -7,10 +7,65 @@ module.exports = (function() {
   var validator = require('validator');
   var parse = require('csv-parse');
 
-  function Downloader() {}
+  function Downloader(eventEmitter, chunkSize) {
+    this.csvData = [];
+    this.readData = [];
+    this.emitter = eventEmitter;
+    this.chunkSize = chunkSize;
+  }
 
+  Downloader.prototype.readCSV = function(file) {
+    var _this = this;
+    var lineReader = require('readline').createInterface({
+      input: fs.createReadStream(file)
+    });
+
+    console.log('Collecting CSV data...');
+
+    lineReader.on('line', (line) => { //read one line at a time
+      parse(line, (err, items) => {
+        if (err) {
+          console.log(line);
+          console.log(err)
+        } else {
+
+          var lineItem = items[0] || [];
+          var url = 'http:' + lineItem[0];
+          //TODO: fix - removing some good URLS - just check url exists?
+          if (validator.isURL(url)) { //if there is a url
+
+            _this.csvData.push({url: url, outputFile: getOutputFile(lineItem)});
+
+          } else {
+
+            // console.log('invalid url: ' + url);
+
+          }
+        }
+      });
+    });
+
+    lineReader.on('close', function() {
+      _this.emitter.emit('csv read complete', _this.csvData);
+    });
+  };
+  //split data into chunks of n
+  Downloader.prototype.getNextDataSet = function(amount) {
+    // var start = nextIndex - _this.readData.length;
+    // var end = (this.csvData.length < start + 10) ? start + 10 : this.csvData.length - start;
+    // var currentData = this.csvData.slice(start, end);
+    // _this.readData.concat(currentData);
+
+    var nextSet = this.csvData.splice(0, amount);
+    console.log(nextSet);
+    this.readData.concat(nextSet);
+    return nextSet;
+  };
+
+  //TODO: now use the array instead of the whole file - loop 20 at a time
   Downloader.prototype.parse = function(file) {
     var _this = this;
+
     var lineReader = require('readline').createInterface({
       input: fs.createReadStream(file)
     });
@@ -28,13 +83,22 @@ module.exports = (function() {
           var url = 'http:' + lineItem[0];
 
           if (validator.isURL(url)) { //if there is a url
-            var outputFile = getOutputFile(lineItem);
 
+            var outputFile = getOutputFile(lineItem);
             var request = http.get(url, (response) => {
+
               _this.handleReseponse(response, outputFile)
+
             }).on('error', (e) => {
+
               console.log(`Got error: ${e.message}`);
-            });;
+
+            });
+          }
+          else {
+
+            console.log('invalid url: ' + url);
+
           }
         }
       });
@@ -57,10 +121,36 @@ module.exports = (function() {
       });
 
     }
-  }
+  };
 
-  Downloader.prototype.download = function(url) {
+  Downloader.prototype.download = function() {
+    var _this = this;
+    var data = this.getNextDataSet(this.chunkSize);
+    var requestCounter = 0;
+    data.forEach( (datum, idx) => {
+      var request = http.get(datum.url, (response) => {
 
+        _this.handleReseponse(response, datum.outputFile)
+
+        response.on('error', (e) => {
+
+          console.log(`Got error: ${e.message}`);
+
+        });
+        
+        response.on('end', () => {
+            //somecounter++ until somecounter === data.length, THEN emit 'send data'
+            requestCounter++;
+            console.log(requestCounter, data.length);
+            if (requestCounter === data.length) {
+
+                _this.emitter.emit('request set complete');
+
+            }
+        });
+
+      });
+    });
   };
 
   Downloader.prototype.save = function(location) {
